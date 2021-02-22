78ca0ae34fe3e1d6bf237e2745d612148b0d4fc5
Tom Sharkey <tbsharkey@gmail.com>
2021-02-22 20:50:23 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,13 +1,7 @@
-require 'pry'
 def hello(array)
   i = 0
-  collection =[]
   while i < array.length
-    collection << yield(array[i])
+    yield(array[i])
     i += 1
   end
-  collection
-    binding.pry
 end
-
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
